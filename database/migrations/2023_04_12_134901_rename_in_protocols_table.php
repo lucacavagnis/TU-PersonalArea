4b6239d11cb2b9b2a395eebeb1c9118a54371d53
Luca Cavagnis <luca.cavagnis.work@gmail.com>
2023-05-03 16:42:12 +0200

    rename bug fixed

--- database/migrations/2023_04_12_134901_rename_in_protocols_table.php
+++ database/migrations/2023_04_12_134901_rename_in_protocols_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('protocol_products', function (Blueprint $table) {
+            $table->dropForeign( 'product_protocol_product_id_foreign');
             $table->renameColumn('product_id','lot_id');
+            $table->foreign('lot_id', 'product_protocol_product_id_foreign')->references('id')->on('physical_products')->onDelete('cascade')->onUpdate('cascade');
+
         });
     }
 
@@ -22,7 +25,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('protocol_products', function (Blueprint $table) {
+            $table->dropForeign( 'product_protocol_product_id_foreign');
             $table->renameColumn('lot_id','product_id');
+            $table->foreign('product_id', 'product_protocol_product_id_foreign')->references('id')->on('physical_products')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 };
