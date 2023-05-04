73c544270a6c8a0dcf5b0dee24b3a643bba8e385
Luca Cavagnis <luca.cavagnis.work@gmail.com>
2023-05-04 11:02:15 +0200

    Builded assets

--- public/build/assets/AddNewLine.902eb09e.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./app.73e76765.js";import"./Button.1ebd24a1.js";const d=({children:e})=>r("div",{className:"flex py-4 pt-4 border-t border-indigo-300 items-end",children:e});export{d as A};
---
+++ public/build/assets/InputError.c7594cfb.js
@@ -0,0 +1,1 @@
+import{a as n}from"./app.4f045999.js";function o({message:r,className:t=""}){return r?n("p",{className:"text-sm text-red-600 "+t,children:r}):null}export{o as I};
